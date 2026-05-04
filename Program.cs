eb7951024ff70c027dd824fba9592c16c0c4bd91
HeitorGonc723 <0001208446@senaimgaluno.com.br>
2026-05-04 06:15:35 -0300

    Variaveis e Operados de Atribuição

--- Program.cs
+++ Program.cs
@@ -1,8 +1,53 @@
-﻿int valor1,valor2;
-valor1 = 10;
-valor2 = 2;
+﻿//Variaveis
+string nome = "Heitor";
+int idade = 23;
+double altura = 1.76;
+Console.WriteLine("Nome: {0}", nome);
+Console.WriteLine("Idade: {0}", idade); 
+Console.WriteLine("Altura: {0}", altura);
 
-Console.WriteLine("Soma: {0}", valor1 + valor2);
-Console.WriteLine("Subtração: {0}", valor1 - valor2);
-Console.WriteLine("Multiplicação: {0}", valor1 * valor2);
-Console.WriteLine("Divisão: {0}", valor1 / valor2);
+
+
+
+//Variáveis e Operadores
+// int valor1,valor2;
+// valor1 = 10;
+// valor2 = 2;
+
+// Console.WriteLine("Soma: {0}", valor1 + valor2);
+// Console.WriteLine("Subtração: {0}", valor1 - valor2);
+// Console.WriteLine("Multiplicação: {0}", valor1 * valor2);
+// Console.WriteLine("Divisão: {0}", valor1 / valor2);
+
+//Operadores de Atribuição
+double a = 10;
+
+Console.WriteLine("Resultado: {0}", a);
+
+a+=3;
+
+Console.WriteLine("Resultado: {0}", a);
+
+a-=3;
+
+Console.WriteLine("Resultado: {0}", a);
+
+a*=3;
+
+Console.WriteLine("Resultado: {0}", a);
+
+a/=3;
+
+Console.WriteLine("Resultado: {0}", a);
+
+a%=3;
+
+Console.WriteLine("Resultado: {0}", a);
+
+a++;
+
+Console.WriteLine("Resultado: {0}", a);
+
+a--;
+
+Console.WriteLine("Resultado: {0}", a);
