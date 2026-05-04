798f5a3c8be7f4c1c770df572853beaa28c0c534
HeitorGonc723 <0001208446@senaimgaluno.com.br>
2026-05-04 06:01:58 -0300

    basics

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿int valor1,valor2;
+valor1 = 10;
+valor2 = 2;
+
+Console.WriteLine("Soma: {0}", valor1 + valor2);
+Console.WriteLine("Subtração: {0}", valor1 - valor2);
+Console.WriteLine("Multiplicação: {0}", valor1 * valor2);
+Console.WriteLine("Divisão: {0}", valor1 / valor2);
